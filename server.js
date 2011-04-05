ae056062d27cf46ba566107f4599ed54832ba510
root <root@li70-243.members.linode.com>
2011-04-05 02:36:56 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,219 @@
+var fu  = require("./fu");
+var sys = require("sys");
+
+var PORT = 8000;
+
+var Room = function(id) {
+	var self = this;
+	self.id = id;
+	self.last_access_time = new Date().getTime();
+	self.messages = {
+		listener: [],
+		venter:   []
+	};
+	self.waiting = {
+		listener: false,
+		venter: false
+	};
+	self.start_timer = 0;
+	self.venter = 0;
+	self.listener = 0;
+	
+	self.poke = function (type) {
+		self.last_access_time = new Date().getTime();
+		if (type) self[type] = new Date().getTime();
+	}
+	
+	self.expired = function () {
+		return self.last_access_time < (new Date().getTime() - (1000 * 30));
+	}
+	
+	self.receive = function (type, response) {
+		self.poke(type);
+		var messages = self.messages[type];
+
+		if (!messages.length) {
+			var timeout = setTimeout(function () {
+				self.waiting[type] = false;
+				response.simpleJSON(200, []);
+			}, 10 * 1000);
+			
+			self.waiting[type] = function() {
+				response.simpleJSON(200, messages);
+				messages.length = 0;
+				self.waiting[type] = false;
+				clearTimeout(timeout);
+			};
+			
+			return;
+		}
+
+		response.simpleJSON(200, messages);
+		messages.length = 0;
+	}
+	
+	self.send = function (type, opposite, message, response) {
+		self.poke(type);
+		
+		var messages = self.messages[opposite];
+		if (message.action == "disconnect") {
+			removeFromWaiters(self.id);
+		}
+
+		messages.push(message);
+		if (self.waiting[opposite]) self.waiting[opposite]();
+
+		response.simpleJSON(200, true);
+	}
+
+	self.start = function () {
+		self.venter = self.listener = new Date().getTime();
+		self.messages["listener"].push({
+			action: "join"
+		});
+		self.messages["venter"].push({
+			action: "join"
+		});
+		if (self.waiting["listener"]) self.waiting["listener"]();
+		if (self.waiting["venter"]) self.waiting["venter"]();
+		
+		self.start_timer = setInterval(function () {
+			var now = new Date().getTime();
+			if ((self.venter < (now - (20 * 1000))) ||
+			    (self.listener < (now - (20 * 1000)))) {
+				self.messages["listener"].push({
+					action: "disconnect"
+				});
+				if (self.waiting["listener"]) self.waiting["listener"]();
+				self.messages["venter"].push({
+					action: "disconnect"
+				});
+				if (self.waiting["venter"]) self.waiting["venter"]();
+				
+				removeFromWaiters(self.id);
+				
+				clearInterval(self.start_timer);
+			}
+		}, 1000 * 2);
+	}
+}
+
+var waiters = {
+	listener: [],
+	venter:   []
+};
+
+function removeFromWaiters(room_id) {
+	var idx = waiters["listener"].indexOf(room_id);
+	if (idx != -1) waiters["listener"].splice(idx, 1);
+	
+	idx = waiters["venter"].indexOf(room_id);
+	if (idx != -1) waiters["venter"].splice(idx, 1);
+}
+
+var rooms = {};
+
+// TODO, get rid of waiters and just look through the active rooms
+// for a room that has the appropriate slot open. It's more brute-
+// forcy, but it makes things simpler as well.
+fu.get("/join", function (request, response) {
+	var type = request.query.type;
+	type = (type == "venter") ? "venter" : "listener";
+	opposite = (type == "venter") ? "listener" : "venter";
+	
+	if (waiters[opposite].length) {
+		// TODO loop through waiters until we find a room that is still defined (exists in rooms)
+		var room_id = waiters[opposite].shift();
+		try {
+			rooms[room_id].start();
+			response.simpleJSON(200, { id: room_id });
+		}
+		catch (e) {
+			console.log("Error starting room:", e);
+			response.simpleJSON(200, {});
+		}
+		return;
+	}
+	
+	var room_id = guid();
+	var tmp_room = new Room(room_id);
+	rooms[room_id] = tmp_room;
+	
+	waiters[type].push(room_id);
+	
+	return response.simpleJSON(200, { id: room_id });
+});
+fu.get("/receive", function (request, response) {
+	var type = request.query.type;
+	type = (type == "venter") ? "venter" : "listener";
+	opposite = (type == "venter") ? "listener" : "venter";
+	var room_id = request.query.rid;
+	
+	var room = rooms[room_id];
+	if (!room) {
+		response.simpleJSON(404, {});
+		return;
+	}
+	
+	return room.receive(type, response);
+});
+fu.get("/send", function (request, response) {
+	var type = request.query.type;
+	type = (type == "venter") ? "venter" : "listener";
+	opposite = (type == "venter") ? "listener" : "venter";
+	var room_id = request.query.rid;
+	var message = {
+		action: request.query.action,
+		data: request.query.data
+	};
+	
+	var room = rooms[room_id];
+	if (!room) {
+		response.simpleJSON(404, {});
+		return;
+	}
+	
+	room.send(type, opposite, message, response);
+})
+fu.get("/counts", function (request, response) {
+	var listeners = 0;
+	var venters = 0;
+	
+	for (var i in rooms) {
+		var room = rooms[i];
+		if (room.listener) listeners += 1;
+		if (room.venter) venters += 1;
+	}
+	
+	response.simpleJSON(200, {
+		listeners: listeners,
+		venters: venters
+	});
+});
+fu.get("/dump", function (request, response) {
+	response.simpleText(200, sys.inspect(rooms));
+})
+
+setInterval(function () {
+	for (i in rooms) {
+		var room = rooms[i];
+		if (room.expired()) {
+			console.log("Removing room", i);
+			delete rooms[i];
+		}
+	}
+}, 1000 * 10);
+
+
+fu.get("/", fu.staticHandler("static/index.html"));
+
+fu.listen(PORT, null);
+
+
+function S4() {
+   return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+}
+function guid() {
+   return (S4()+S4()+S4()+S4()+S4()+S4());
+}
+
---
+++ static/chat.js
@@ -0,0 +1,275 @@
+$.extend({
+	getUrlVars: function(){
+		var vars = [], hash;
+		var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+		for(var i = 0; i < hashes.length; i++)
+		{
+			hash = hashes[i].split('=');
+			vars.push(hash[0]);
+			vars[hash[0]] = hash[1];
+		}
+		return vars;
+	},
+	getUrlVar: function(name){
+		return $.getUrlVars()[name];
+	}
+});
+
+function info(msg) {
+	status(msg, 'infoMessage');
+}
+
+function error(msg) {
+	status(msg, 'errorMessage');
+}
+
+function status(msg, cssClass) {
+	
+	
+	if(msg == '') {
+		msg = '<form id="msgForm"><input id="chatInput" type="text" size=90 /><input type="submit" value="Send Chat"  /></form>';
+		msgform = true;
+	}
+	else {
+		msgform = false;
+	}
+	
+	var status = $('#status');
+	status.removeClass('errorMessage infoMessage');
+	status.addClass(cssClass);
+	status.html(msg);
+	if(msgform) {
+		$('#chatInput').focus();
+		$('#msgForm').submit(
+				function() {
+					sendMessage();
+					return false;
+				}
+			);
+	}
+	
+}
+
+var chatId = -1;
+var other;
+var hasPartner = false;
+
+function newPartner(cb, sync) {
+	if(hasPartner) {
+		var oldChatId = chatId;
+		chatId = -1;
+		hasPartner = false;
+		$.getJSON(
+			'/send', {
+				rid: oldChatId,
+				type: $.getUrlVar('type'),
+				action: "disconnect"
+			},
+			cb
+		);
+	}
+}
+
+var hasFocus = true;
+$(window).bind("blur", function() { 
+	hasFocus = false;
+});
+$(window).bind("focus", function() {
+	hasFocus = true;
+	document.title = 'CompassionPit | Chat'; 
+});
+
+$(document).ready(
+	function() {
+		$('#enable_sound').attr('checked', true);
+		info('Initializing');
+		
+		try {
+			function audioReady() {
+				this.element.jPlayer('setFile', '/gong.mp3');
+			}
+			$('#audioPlayer').jPlayer({
+				ready: audioReady, 
+				swfPath: '/', 
+				reload: 'auto'
+			});
+		} catch(e) {
+		}
+		
+		$('#msgForm').submit(
+				function() {
+					sendMessage();
+					return false;
+				}
+			);
+		
+		$('#newPartner').click(
+				function() {
+					newPartner(
+							function(data) {
+								addMessage('System', 'Please wait while we find you a new chat partner.');
+								getPartner()
+							}
+						);
+				}
+			);
+		
+		// window.onbeforeunload = function(event) {
+		// 	return 'You sure?';
+		// }
+		$(window).unload(function () {
+			$.ajax({
+				url: "/send",
+				async: false,
+				data: {
+					rid: chatId,
+					type: $.getUrlVar('type'),
+					action: "disconnect"
+				}
+			});
+		})
+			
+		other = ($.getUrlVar('type') == 'listener') ? 'Venter' : 'Listener';
+		
+		getPartner();
+	}
+);
+
+
+function getPartner() {
+	hasPartner = false;
+	$.getJSON(
+			'/join?type=' + $.getUrlVar('type'), 
+			function(data) {
+				initChat(data);
+			}
+		);
+	info('Waiting for a chat partner... ');
+}
+
+function initChat(data) {
+	if (!data) {
+		getPartner();
+		return;
+	}
+	// console.log("initChat", data);
+	chatId = data.id;
+	
+	getMessages();
+}
+
+function gong() {
+	try {
+		$('#audioPlayer').jPlayer('playHead', 0);
+		$('#audioPlayer').jPlayer('play');
+	} catch(err) {
+	}
+}
+
+var count = 0;
+var i = 0;
+var titleCurrentlyChanging = false;
+function addMessage(from, msg) {
+	var cls = ((count++ & 1) == 0) ? 'chatMessageEven' : 'chatMessageOdd';
+	var row = $('#chatWindow > tbody:last').append('<tr class="' + cls + '"><td>' + from + ': ' + msg + '</td></tr>');
+	var scrollDiv = document.getElementById("column_left_chat"); //scroll to bottom of chat
+	scrollDiv.scrollTop = scrollDiv.scrollHeight;	
+	if(!hasFocus && !titleCurrentlyChanging) {
+		changeTitle();
+		if($("#enable_sound").is(':checked')) {
+			gong();
+		}
+	}
+}
+
+function changeTitle() {
+	i++;
+	if(i%2) {
+		document.title = 'New message on CompassionPit!';
+	}
+	else {
+		document.title = 'CompassionPit | Chat';
+	}
+	if(!hasFocus) {
+		titleCurrentlyChanging = true;
+		setTimeout('changeTitle()',1000);
+	}
+	else {
+		titleCurrentlyChanging = false;
+		i=0;
+		document.title = 'CompassionPit | Chat';
+	}
+}
+
+function getMessages() {
+	if (chatId == -1) return;
+	var curChatId = chatId;
+	$.getJSON(
+			'/receive', {
+				rid: chatId,
+				type: $.getUrlVar('type')
+			}, 
+			function(data) {
+				// console.log("getMessages", data);
+				// if(data == false) {
+				// 	addMessage('System', 'Your chat partner got disconnected. Please wait while we find you a new ' + other + '.');
+				// 	return getPartner();
+				// }
+				// else if(data == true) {
+				// 	info('');
+				// 	addMessage('System', 'A new chat partner has entered your chat (#' + (chatId >> 1) + ').');
+				// 	hasPartner = true;
+				// }
+				if(data.length)
+					handleMessages(data);
+				
+				getMessages();
+			}
+		)
+}
+
+function handleMessages(messages) {
+	messages.forEach(function (message) {
+		switch (message.action) {
+			case "join":
+				info('');
+				addMessage('System', 'A new chat partner has entered your chat');
+				hasPartner = true;
+				break;
+			case "message":
+				addMessage(other, message.data);
+				break;
+			case "disconnect":
+				addMessage("System", "Your chat partner disconnected, please wait while we find you a new " + other + ".");
+				chatId = -1;
+				getPartner();
+				break;
+			default:
+				console.log("Unhandled message", message);
+		}
+	})
+}
+
+function sendMessage() {
+	var msg = $('#chatInput').val();
+	if(msg == '' || chatId == -1)
+		return;
+	info('Sending message...')
+	$.getJSON(
+			'/send', {
+				rid: chatId,
+				type: $.getUrlVar('type'),
+				action: "message",
+				data: msg
+			},
+			function(data) {
+				if(data == true) {
+					addMessage('Me', msg);
+					info('');
+					$('#chatInput').val('')
+				} else {
+					error('Failed to send message.')
+				}
+			}
+		);
+}
