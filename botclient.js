c83ab458d81cd58b407cd8aeb6a4da7c08880dc1
Swizec Teller <swizec@swizec.com>
2011-04-14 18:50:20 +0200

    bot just having some issues connecting to the right room

--- botclient.js
+++ botclient.js
@@ -1,5 +1,6 @@
 
-var http = require('http');
+var http = require('http'),
+    querystring = require('querystring');
 
 var Client = function (type) {
     var self = this;
@@ -13,19 +14,61 @@ var Client = function (type) {
 	var options = self.options;
 	options.path = '/join';
 
+	self.get(options, function (data) {
+	    data = JSON.parse(data);
+	    self.room_id = data.id;
+	    console.log(self.type, "Connected to '"+self.room_id+"'");
+	});
+    };
+
+    self.receive = function () {
+	console.log(self.type, "receiving ...");
+
+	var options = self.options;
+	options.path = '/receive?'+querystring.stringify({rid: self.room_id,
+							  type: self.type});
+	self.get(options, function (data) {
+	    data = JSON.parse(data);
+	    console.log(self.type, "Messages: ", data);
+
+	    self.receive();
+	});
+    };
+
+    self.send = function () {
+	var options = self.options;
+	options.path = '/send?'+querystring.stringify({rid: self.room_id,
+						       type: self.type,
+						       action: 'message',
+						       data: "tiem: "+(new Date()).getTime()});
+	self.get(options, function (data) {
+	    console.log(self.type, "Said something");
+	});
+    };
+
+    self.get = function (options, callback) {
 	http.get(options, function (res) {
 	    var data = '';
 	    res.on('data', function (chunk) { data += chunk });
 	    res.on('end', function () {
-		data = JSON.parse(data);
-		self.room_id = data.id;
-		console.log("Connected to '"+self.room_id+"'");
+		callback(data);
 	    });
 	}).on('error', function (e) {
-	    console.log('Join error: ', e);
+	    console.log(self.type, 'Error: ', e);
 	});
-    }
+
+    };
 };
 
 var venter = new Client('venter');
 venter.connect();
+var listener = new Client('listener');
+listener.connect();
+
+setTimeout(function () {
+    venter.receive();
+    listener.receive();
+    setInterval(function () {
+	venter.send();
+    }, 1000);
+}, 1000);
