d9d988d84f92fa293dd76844906e610593bd93c8
Cameron Kenneth Knight <ckknight@gmail.com>
2011-04-26 03:49:00 -0500

    Make the config change based on NODE_ENV.

--- config.js
+++ config.js
@@ -1,10 +1,24 @@
-module.exports = {
-    port: 8000,
-    nowjsPort: 8000,
-    nowjsHost: "localhost",
-    logLimits: {
-        info: 1000,
-        warn: 1000,
-        error: 1000
+var config = {
+    development: {
+        port: 8000,
+        nowjsPort: 8000,
+        nowjsHost: "localhost",
+        logLimits: {
+            info: 1000,
+            warn: 1000,
+            error: 1000
+        }
+    },
+    production: {
+        port: 8000,
+        nowjsPort: 80,
+        nowjsHost: "compassionpit.com",
+        logLimits: {
+            info: 1000,
+            warn: 1000,
+            error: 1000
+        }
     }
 };
+
+module.exports = config[process.env.NODE_ENV || "development"];
