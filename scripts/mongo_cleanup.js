bbe216aeac5e2f954ca8705b325a44f86952659d
Aleksandr Guidrevitch <aguidrevitch@gmail.com>
2011-09-18 19:24:53 +0300

    mongo cleanup intervals refined

--- scripts/mongo_cleanup.js
+++ scripts/mongo_cleanup.js
@@ -1,6 +1,10 @@
 (function () {
     "use strict";
 
+    var CONVERSATIONS_HISTORY = 30; // days
+    var LOG_ENTRIES_HISTORY = 7; // days
+    var CLIENT_ERRORS_HISTORY = 7; // days
+
     require('date-utils');
     var mongoose = require('mongoose'),
          config  = require("../config");
@@ -10,19 +14,18 @@
     var Conversation = mongoose.model('Conversation', new mongoose.Schema({}));
     var LogEntries   = mongoose.model('LogEntries', new mongoose.Schema({}));
     var ClientErrors = mongoose.model('ClientErrors', new mongoose.Schema({}));
-    var startTime = (new Date()).add({ days: -30 });
 
-    Conversation.remove({ finishTime: { $lt : startTime }, finishReason : { $nin : [ "venterReportedAbuse", "listenerReportedAbuse" ] }  }, function (err) {
+    Conversation.remove({ finishTime: { $lt : (new Date()).add({ days: -1 * CONVERSATIONS_HISTORY }) }, finishReason : { $nin : [ "venterReportedAbuse", "listenerReportedAbuse" ] }  }, function (err) {
         if (err) {
             console.log(err.toString());
             process.exit(1);
         } else {
-            LogEntries.remove({ time: { $lt : startTime } }, function (err) {
+            LogEntries.remove({ time: { $lt : (new Date()).add({ days: -1 * LOG_ENTRIES_HISTORY}) } }, function (err) {
                 if (err) {
                     console.log(err.toString());
                     process.exit(1);
                 } else {
-                    ClientErrors.remove({ time: { $lt : startTime } }, function (err) {
+                    ClientErrors.remove({ time: { $lt : (new Date()).add({ days: -1 * CLIENT_ERRORS_HISTORY }) } }, function (err) {
                         if (err) {
                             console.log(err.toString());
                             process.exit(1);
