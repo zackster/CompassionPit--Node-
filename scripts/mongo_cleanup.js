5060025fb2e7ad424c31df37e0f9dbc24628ad3d
Aleksandr Guidrevitch <aguidrevitch@gmail.com>
2011-09-19 17:42:17 +0300

    mongo cleanup intervals tweaked

--- scripts/mongo_cleanup.js
+++ scripts/mongo_cleanup.js
@@ -1,9 +1,9 @@
 (function () {
     "use strict";
 
-    var CONVERSATIONS_HISTORY = 30; // days
-    var LOG_ENTRIES_HISTORY = 7; // days
-    var CLIENT_ERRORS_HISTORY = 7; // days
+    var CONVERSATIONS_HISTORY = 180 * 24; // hours
+    var LOG_ENTRIES_HISTORY = 7 * 24; // hours
+    var CLIENT_ERRORS_HISTORY = 1; // hours
 
     require('date-utils');
     var mongoose = require('mongoose'),
@@ -15,17 +15,17 @@
     var LogEntries   = mongoose.model('LogEntries', new mongoose.Schema({}));
     var ClientErrors = mongoose.model('ClientErrors', new mongoose.Schema({}));
 
-    Conversation.remove({ finishTime: { $lt : (new Date()).add({ days: -1 * CONVERSATIONS_HISTORY }) }, finishReason : { $nin : [ "venterReportedAbuse", "listenerReportedAbuse" ] }  }, function (err) {
+    Conversation.remove({ finishTime: { $lt : (new Date()).add({ hours: -1 * CONVERSATIONS_HISTORY }) }, finishReason : { $nin : [ "venterReportedAbuse", "listenerReportedAbuse" ] }  }, function (err) {
         if (err) {
             console.log(err.toString());
             process.exit(1);
         } else {
-            LogEntries.remove({ time: { $lt : (new Date()).add({ days: -1 * LOG_ENTRIES_HISTORY}) } }, function (err) {
+            LogEntries.remove({ time: { $lt : (new Date()).add({ hours: -1 * LOG_ENTRIES_HISTORY}) } }, function (err) {
                 if (err) {
                     console.log(err.toString());
                     process.exit(1);
                 } else {
-                    ClientErrors.remove({ time: { $lt : (new Date()).add({ days: -1 * CLIENT_ERRORS_HISTORY }) } }, function (err) {
+                    ClientErrors.remove({ time: { $lt : (new Date()).add({ hours: -1 * CLIENT_ERRORS_HISTORY }) } }, function (err) {
                         if (err) {
                             console.log(err.toString());
                             process.exit(1);
