afa977366e54d6d60d574d2685c29be648710e9f
Zachary Burt <zackster@gmail.com>
2012-03-10 01:57:53 -0800

    debugging mongo script..

--- scripts/mongo_stats.js
+++ scripts/mongo_stats.js
@@ -37,7 +37,7 @@
                   var user_connects = connections[doc.userid] ? connections[doc.userid] : [];
                   console.log(user_connects);
                   console.log(doc.time);
-                  console.log(doc);
+                  console.log(doc['time']);
                   console.log(new Date(doc.time));
                   user_connects.push(doc.time.toString());
                   connections[doc.userid] = user_connects;
