09ab675a33cb17b2e0b9948979bf5568bff1fc43
Zachary Burt <zackster@gmail.com>
2012-03-10 02:08:26 -0800

    debugging mongo script..

--- scripts/mongo_stats.js
+++ scripts/mongo_stats.js
@@ -14,7 +14,10 @@
     mongoose.connect(config.mongodb.uri);
     console.log(config.mongodb.uri);
     //    var Conversation = mongoose.model('Conversation', new mongoose.Schema({}));
-    var LogEntries = mongoose.model('LogEntries', new mongoose.Schema({}));
+    var LogEntries = mongoose.model('LogEntries', new mongoose.Schema({
+      userid: { type: String },
+      time: { type: Date }
+    }));
 
     var connections = {};
     var disconnections = {};
@@ -33,24 +36,10 @@
             if (docs) {
               var doc;
               for (var i in docs) {
-                  console.log(docs[i]);
-                  
                   doc = docs[i];
-                  console.log("DOC", doc);
-                  console.log(doc.prototype);
-                  console.log(typeof(doc));
                   var user_connects = connections[doc.userid] ? connections[doc.userid] : [];
-                  console.log(user_connects);
-                  console.log(doc.time);
-                  console.log(doc['time']);
-                  console.log(docs[i]['time']);
-                  console.log(docs[i].userid);
-                  console.log(doc[0]);
-                  console.log(doc.userid);
-                  console.log(new Date(doc.time));
                   user_connects.push(doc.time.toString());
                   connections[doc.userid] = user_connects;
-                  console.log(user_connects);
               }
               console.log('connections');
               console.log(connections);
