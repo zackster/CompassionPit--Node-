310a8e78869af3de16c28566e9bacc2286830044
Zachary Burt <zackster@gmail.com>
2012-03-10 01:26:48 -0800

    debugging mongo script..

--- scripts/mongo_stats.js
+++ scripts/mongo_stats.js
@@ -3,9 +3,9 @@
 
 
 
-    var CONVERSATIONS_HISTORY = 180 * 24; // hours
-    var LOG_ENTRIES_HISTORY = 7 * 24; // hours
-    var CLIENT_ERRORS_HISTORY = 1; // hours
+    // var CONVERSATIONS_HISTORY = 180 * 24; // hours
+    // var LOG_ENTRIES_HISTORY = 7 * 24; // hours
+    // var CLIENT_ERRORS_HISTORY = 1; // hours
 
     require('date-utils');
     var mongoose = require('mongoose'),
@@ -13,30 +13,24 @@
 
     mongoose.connect(config.mongodb.uri);
     console.log(config.mongodb.uri);
-    var Conversation = mongoose.model('Conversation', new mongoose.Schema({}));
+//    var Conversation = mongoose.model('Conversation', new mongoose.Schema({}));
     var LogEntries   = mongoose.model('LogEntries', new mongoose.Schema({}));
     
     var connections = {};
     var disconnections = {};
 
-    LogEntries.count({}, function(err,docs) {
-      console.log(err);
-      console.log(docs);
-    });
+
 
     LogEntries.find({action:'connect'}, ['userid', 'time'], function(err, docs) {
-            console.log('hi');
       if(err) {  throw err; }
       for(var i in docs) {
         var doc = docs[i];
         connections[doc.userid].push(doc.time);
       }
-      console.log(connections.length);
     });
     
     LogEntries.findOne({action:'disconnect'}, ['userid', 'time'], function(err, docs) {
-      console.log('hi');
-      if(err) {  throw err; }              console.log('done');
+      if(err) {  throw err; }
       for(var i in docs) {
         var doc = docs[i];
         disconnections[doc.userid].push(doc.time);
