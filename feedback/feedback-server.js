3f06ffbf92438d7effa9aab1686b20197499ff1e
Zachary Burt <zackster@gmail.com>
2012-03-13 00:20:25 -0700

    leaderboard changes...

--- feedback/feedback-server.js
+++ feedback/feedback-server.js
@@ -113,9 +113,7 @@
       });
     },
     getLeaderboard: function(cb) {
-      console.log(cb.toString());
-      // console.log(this.mostRecentScores);
-      cb(this.mostRecentScores);
+      cb.apply(this, this.mostRecentScores);
     }
 
   };
