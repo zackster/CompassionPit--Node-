6a5d5043af06b00482644540fe026b7b99a07978
Zachary Burt <zackster@gmail.com>
2012-03-17 19:21:30 -0700

    authentication server updates for VB sso

--- authentication/auth-server.js
+++ authentication/auth-server.js
@@ -48,6 +48,7 @@
 
     Server.prototype.checkLogin = function(req, callback) {
       var self = this; // Server context
+      console.log(req.cookies);
       this.getCookie(req.cookies.bb_userid, req.cookies.bb_password, function(user) {
         if(user) {
           self.markLoggedIn(req.cookies.bb_userid);
