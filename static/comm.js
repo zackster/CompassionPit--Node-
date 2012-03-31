585d522350ea3262bc61e5ec9d2e58a3f905702c
Zachary Burt <zackster@gmail.com>
2012-03-31 06:25:53 -0700

    socketio bugsquashing

--- static/comm.js
+++ static/comm.js
@@ -84,7 +84,7 @@
 
         if (/MSIE (\d+\.\d+);/.test(navigator.userAgent)){
             socket = io.connect(socketio_addr,{
-                    'transports': ['xhr-polling', 'flashsocket'],
+                    'transports': ['flashsocket','htmlfile'],
                     'max reconnection attempts': 15,
                     'force new connection': true
             });
